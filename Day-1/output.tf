8702b5602ff739d6507717fada011ea944652cb1
govindgupta01072019 <govindgupta01072019@gmail.com>
2026-02-22 16:28:12 +0530

    2nd commit

--- Day-1/output.tf
+++ Day-1/output.tf
@@ -2,3 +2,7 @@ output "instance_public_ip" {
   description = "Public IP of EC2 instance"
   value       = aws_instance.my_ec2.public_ip
 }
+
+output "instance_private_IP" {
+  value = aws_instance.my_ec2.private_ip
+}
